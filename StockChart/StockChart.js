eb258e75855f27f66b5291a4654d269c01bb8acd
Kentkn192837 <agriculture5012@gmail.com>
2022-11-14 00:03:17 +0900

    ローソク足の描画

--- StockChart/StockChart.js
+++ StockChart/StockChart.js
@@ -1,3 +1,28 @@
+const bar = (ctx) => {
+  const start_x = 300;
+  const start_y = 250;
+
+  const bar_width = 30;
+  const bar_height = 30;
+  ctx.strokeStyle = '#0f0';
+  ctx.fillStyle = '#0f0';
+  ctx.beginPath();
+  ctx.moveTo(start_x, start_y);
+  ctx.lineTo(start_x + bar_width, start_y);
+
+  ctx.moveTo(start_x + bar_width / 2, start_y);
+  ctx.lineTo(start_x + bar_width / 2, start_y + bar_height);
+  
+  ctx.fillRect(start_x, start_y + bar_height, bar_width, bar_height + 40);
+  
+  ctx.moveTo(start_x + bar_width / 2, start_y + bar_height + 40);
+  ctx.lineTo(start_x + bar_width / 2, start_y + bar_height + 80);
+
+  ctx.moveTo(start_x, start_y + bar_height + 80);
+  ctx.lineTo(start_x + bar_width, start_y + bar_height + 80)
+  ctx.stroke();
+}
+
 const canvasInit = (canvas) => {
   const ctx = canvas.getContext("2d");
 
@@ -13,4 +38,5 @@ window.onload = function() {
   const ctx = document.getElementById("canvas").getContext("2d");
   
   canvasInit(canvas);
+  bar(ctx);
 }
