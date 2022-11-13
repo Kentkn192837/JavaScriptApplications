37c19623d18a6deea33b1e9011d8be662bd23db5
Kentkn192837 <agriculture5012@gmail.com>
2022-11-14 03:38:45 +0900

    上ひげ, 下ひげ, 始値と終値が同じときのローソクの処理

--- StockChart/StockChart.js
+++ StockChart/StockChart.js
@@ -1,26 +1,54 @@
-const bar = (ctx) => {
-  const start_x = 300;
-  const start_y = 250;
-
+const bar = (ctx, start_x, start_y, openingQuotation, closingQuotation, highest, lowest) => {
   const bar_width = 30;
-  const bar_height = 30;
-  ctx.strokeStyle = '#0f0';
-  ctx.fillStyle = '#0f0';
+  const bar_height = Math.abs(closingQuotation - openingQuotation);
+
+  const bar_center = start_x + bar_width / 2;
+  const upper_line = Math.abs(highest - closingQuotation);        // 上ひげの長さ
+  const under_line = Math.abs(openingQuotation - lowest);   // 下ひげの長さ
+
+  if (closingQuotation > openingQuotation) {
+    ctx.strokeStyle = '#0f0';
+    ctx.fillStyle = '#0f0';
+  } else if (closingQuotation == openingQuotation) {
+    ctx.strokeStyle = '#fff';
+    ctx.fillStyle = '#fff';
+  } else {
+    ctx.strokeStyle = '#f00';
+    ctx.fillStyle = '#f00';
+  }
+
   ctx.beginPath();
-  ctx.moveTo(start_x, start_y);
-  ctx.lineTo(start_x + bar_width, start_y);
-
-  ctx.moveTo(start_x + bar_width / 2, start_y);
-  ctx.lineTo(start_x + bar_width / 2, start_y + bar_height);
+  // 上ひげ
+  ctx.moveTo(bar_center, start_y);
+  ctx.lineTo(bar_center, start_y - upper_line);
   
-  ctx.fillRect(start_x, start_y + bar_height, bar_width, bar_height + 40);
-  
-  ctx.moveTo(start_x + bar_width / 2, start_y + bar_height + 40);
-  ctx.lineTo(start_x + bar_width / 2, start_y + bar_height + 80);
+  ctx.fillRect(start_x, start_y, bar_width, bar_height);
 
-  ctx.moveTo(start_x, start_y + bar_height + 80);
-  ctx.lineTo(start_x + bar_width, start_y + bar_height + 80)
+  if (closingQuotation == openingQuotation) {
+    const canvas_height = document.getElementById("canvas").height;
+    ctx.moveTo(start_x, canvas_height - openingQuotation);
+    ctx.lineTo(start_x + bar_width, canvas_height - openingQuotation);
+  }
+  
+  // 下ひげ
+  ctx.moveTo(bar_center, start_y + bar_height);
+  ctx.lineTo(bar_center, start_y + under_line);
   ctx.stroke();
+}
+
+
+const draw = (ctx) => {
+  let x = 0;
+  let i = 0;
+  while (i < 10) {
+    bar(ctx, x, 50, 80, 120, 200, 250);
+    x += 31;
+    i++;
+  }
+
+  bar(ctx, 20, 200, 150, 120, 200, 250);
+  bar(ctx, 430, 100, 300, 250, 90, 250);
+  bar(ctx, 300, 400, 150, 150, 80, 250);
 }
 
 const canvasInit = (canvas) => {
@@ -38,5 +66,5 @@ window.onload = function() {
   const ctx = document.getElementById("canvas").getContext("2d");
   
   canvasInit(canvas);
-  bar(ctx);
+  draw(ctx);
 }
